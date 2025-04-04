bd18a67fb454c7a8c77294b20d6dda5ca4a6f529
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-04-04 23:13:27 +0530

    Create data.js

---
+++ utilities/data.js
@@ -0,0 +1,29 @@
+ const data = {
+    title:"pizza hut",
+    imageUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/11c864b1-8f68-465d-b012-54538f2143d5_14779.jpg",
+    foods:[
+        {
+            dishName:"Chicken Pizza",
+            price:500,
+
+        },
+    ],
+    time: "9am to 9pm",
+    pickup:true,
+    delivery:true,
+    isOpen:true,
+    logoUrl:"",
+    rating:5,
+    ratingCount:"5",
+    code:"1234",
+    coords:{
+        id:"123456",
+        latitude:"1234",
+        latitudeDelta:"1234",
+        longitude:"1234",
+        longitudeDelta:"1234",
+        address:"Mumbai India",
+    },
+
+
+}
